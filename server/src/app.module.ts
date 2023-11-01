37ecb4823b561a14235d43b2c1429fbc2a595440
Anton Zamay <tony.zamay@mail.ru>
2023-11-01 01:35:02 +0100

    env variables, uri to mongo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from "@nestjs/mongoose";
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.vcsomdf.mongodb.net/?retryWrites=true&w=majority`
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(uri),
     TrackModule
   ],
 })
