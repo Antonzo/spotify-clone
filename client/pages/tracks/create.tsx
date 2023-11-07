ca06f8dcb590778f5bd707c8f59bc8c41ffadea0
Anton Zamay <tony.zamay@mail.ru>
2023-11-07 15:45:51 +0100

    create track page skeleton

---
+++ client/pages/tracks/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../../layouts/DefaultLayout'
+
+const CreateTrackPage = () => {
+  return (
+    <DefaultLayout>
+      <h1>Create Track</h1>
+    </DefaultLayout>
+  )
+}
+
+export default CreateTrackPage
