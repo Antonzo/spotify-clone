0ad74a01f02917c6d9f6b231065e37f306086cdf
Anton Zamay <tony.zamay@mail.ru>
2023-11-07 18:59:54 +0100

    progress nav

--- client/components/Player/index.tsx
+++ client/components/Player/index.tsx
@@ -1,5 +1,6 @@
-import { IconButton } from '@material-ui/core';
-import { Pause, PlayArrow } from '@material-ui/icons';
+import { Grid, IconButton } from '@mui/material';
+import { Pause, PlayArrow, VolumeUp } from '@mui/icons-material';
+import TrackProgress from 'components/TrackProgress';
 import styles from './Player.module.scss';
 
 const Index = () => {
@@ -10,6 +11,17 @@ const Index = () => {
       <IconButton onClick={(e) => e.stopPropagation()}>
         {active ? <Pause /> : <PlayArrow />}
       </IconButton>
+      <Grid
+        container
+        direction="column"
+        style={{ width: 200, margin: '0 20px' }}
+      >
+        <div>{track.name}</div>
+        <div style={{ fontSize: 12, color: 'gray' }}>{track.artist}</div>
+      </Grid>
+      <TrackProgress left={0} right={100} onChange={() => {}} />
+      <VolumeUp style={{ marginLeft: 'auto' }} />
+      <TrackProgress left={0} right={100} onChange={() => {}} />
     </div>
   );
 };
